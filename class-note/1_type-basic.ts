2870c08a81925ba3b314242a854e00996df45528
seongwon <87584033+Magiof@users.noreply.github.com>
2022-04-02 20:08:56 +0900

    DEL: clear

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -9,7 +9,7 @@ let num: number = 10;
 
 // ts 배열
 let arr: Array<number> = [1, 2, 3];
-let heroes: Array<string> = ["Capt", "Thor", "Hulk", 10];
+let heroes: Array<string> = ["Capt", "Thor", "Hulk"];
 let items: number[] = [1, 2, 3];
 
 // ts 튜플
