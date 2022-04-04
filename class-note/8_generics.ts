feddfc38706c33e1c51be736b56781d2270e2d4a
seongwon <87584033+Magiof@users.noreply.github.com>
2022-04-04 16:28:09 +0900

    ADD: ~ quiz2

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -37,11 +37,45 @@ function logText<T>(text: T): T {
   return text;
 }
 
-const str1 = logText<string>('abc');
-str1.split('')
+const str1 = logText<string>("abc");
+str1.split("");
 logText<boolean>(true);
 
-logText('a')
-logText(10)
+logText("a");
+logText(10);
 
 // ★generic은 '함수를 정의할 때'가 아닌 '함수를 호출할 때' 타입을 정의한다.
+
+// 제네릭 타입 제한
+// function logTextLength<T>(text: T[]): T[] {
+//   console.log(text.length);
+//   text.forEach(function (text) {
+//     console.log(text);
+//   });
+//   return text;
+// }
+// logTextLength(["hi", "abc"]);
+
+// 제네틱 타입 제한 2 - 정의된 타입 이용하기
+interface LengthType {
+  length: number;
+}
+function logTextLength<T extends LengthType>(text: T): T {
+  text.length;
+  return text;
+}
+logTextLength(10);
+logTextLength( {length: 10});
+
+// 제네릭 타입 제한 3 - keyof
+interface ShoppingItem{
+  name: string;
+  price: number;
+  stock: number;
+}
+function getShoppingItemOption<T extends keyof ShoppingItem>(itemOption: T): T {
+  return itemOption;
+}
+// getShoppingItemOption(10);
+// getShoppingItemOption<string>('a');
+getShoppingItemOption('name');
